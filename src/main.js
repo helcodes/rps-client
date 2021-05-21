cee48bc86c3f5f266becef5e331a8e4cd02cf54a
helcodes <helenehh8@gmail.com>
2021-05-21 11:39:57 +0200

    xx

--- src/main.js
+++ src/main.js
@@ -4,6 +4,9 @@ import router from '@/router'
 import VueSocketIO from 'vue-socket.io'
 import SocketIO from 'socket.io-client'
 
+//https://sable-nebulous-gondola.glitch.me/
+
+
 //const url='http://localhost:8081'
 const url = `${window.location.protocol}//${window.location.hostname}:8081`
 //string interpolation `${javascript-code}`
