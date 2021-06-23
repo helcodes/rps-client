6699bb54f03d30a3860af3289e3761b0b1d233cf
helcodes <helenehh8@gmail.com>
2021-06-23 21:00:10 +0200

    models

--- src/main.js
+++ src/main.js
@@ -4,6 +4,7 @@ import router from '@/router'
 import VueSocketIO from 'vue-socket.io'
 import SocketIO from 'socket.io-client'
 import store from './store'
+import vuetify from './plugins/vuetify'
 
 //const url='http://localhost:8081'
 let url = `${window.location.protocol}//${window.location.hostname}:8081`
@@ -36,5 +37,6 @@ Vue.config.productionTip = false
 new Vue({
   router,
   store: store,
-  render: h => h(App),
+  vuetify,
+  render: h => h(App)
 }).$mount('#app')
--- src/views/Game.vue
+++ src/views/Game.vue
@@ -1,16 +1,33 @@
 <template>
-    <div>
-        <button @click="logout">Logout</button>
-    </div>
+    <v-card style="width: 60%" class="mx-auto">
+        <div v-if="screen == 'idle'">
+            <idle />
+        </div>
+        <div v-else-if="screen == 'match'">
+            <match />
+        </div>
+        <div v-else-if="screen == 'result'">
+            <result/>
+        </div>
+    </v-card>
 </template>
 
 <script>
+import Match from '@/components/Match'
+import Idle from '@/components/Idle'
+import Result from '@/components/Result'
+
 export default {
   name: 'Game',
-  methods: {
-    logout() {
-      this.$store.dispatch('logout')
-    }
+  components: {
+      Match,
+      Idle,
+      Result
+  },
+  computed: {
+      screen () {
+          return this.$store.state.screen
+      }
   }
 }
 </script>
